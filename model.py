1a26efd3199f97f4abde587867488405baed9cab
Medellin Qin <Medellin@github.com>
2018-04-17 10:39:45 -0400

    success

--- model.py
+++ model.py
@@ -1,16 +1,21 @@
-import json
 from datetime import datetime
+import json
 
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+next_id = 0
 
-def init(app):
-    global entries
+def init():
+    global entries,next_id,GUESTBOOK_ENTRIES_FILE
+
     try:
-
-        f = open(GUESTBOOK_ENTRIES_FILE)
-        entries = json.loads(f.read())
+        # next_id should end up being set to one greater than the maximum ID found in the previous entry data
+        with open(GUESTBOOK_ENTRIES_FILE) as f:
+            entries = json.load(f)
+        for each_dict in entries:
+            if next_id <= int(each_dict['id']) :
+                next_id = int(each_dict['id'])+ 1
         f.close()
     except:
         print('Couldn\'t open', GUESTBOOK_ENTRIES_FILE)
@@ -21,15 +26,33 @@ def get_entries():
     return entries
 
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+    global entries, GUESTBOOK_ENTRIES_FILE,next_id
     now = datetime.now()
     time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    entry = {"author": name, "text": text, "timestamp": time_string, "id": next_id}
     entries.insert(0, entry) ## add to front of list
+    next_id +=1
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
         dump_string = json.dumps(entries)
         f.write(dump_string)
+        f.close()
+    except:
+        print("ERROR! Could not write entries to file.")
+
+def delete_entry(id):
+    global entries, GUESTBOOK_ENTRIES_FILE, next_id
+    for i in range(len(entries)):
+        if entries[i]['id'] == int(id):
+            entries.pop(i)
+            break
+    next_id -=1
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        dump_string = json.dumps(entries)
+        f.write(dump_string)
+        # for element in entries:
+        #     print(element)
         f.close()
     except:
         print("ERROR! Could not write entries to file.")
